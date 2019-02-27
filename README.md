1e4bcc485a3e9627b51caf8d3c2040d6a6981788
Lanahh <iloveitsdev@gmail.com>
2019-02-27 14:43:00 -0500

    upload project

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# practice-IELTS-listening
-Nowadays, we have IELTS Test on computer, and on Youtube there are providing IELTS LISTENING PRACTICE TEST. I do pack them together. 
-#Challenge 
-Do practice test everyday.
-Calculate your IELTS band score.
-Take note.
-
+# practice-IELTS-listening-test
---
+++ script.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    
+    $('#btnCal').click(function () {
+        let numberNotChecked = $('input:checkbox:not(":checked")').length;
+        let correctAns = 40 - numberNotChecked;
+        let bandScore = 4;
+        let minScore = 11;
+        //calculate minScore+++ , bandScore = 4 +.5
+            if(12<correctAns){
+                bandScore = 4.5
+            } 
+            if(15<correctAns){
+                bandScore = 5
+            } 
+            if(17<correctAns){
+                bandScore = 5.5
+            } 
+            if(22<correctAns){
+                bandScore = 6
+            } 
+            if(25<correctAns){
+                bandScore = 6.5
+            } 
+            if(29<correctAns){
+                bandScore = 7
+            } 
+            if(31<correctAns){
+                bandScore =  7.5
+            } 
+            if(34<correctAns){
+                bandScore =  8
+            } 
+            if(36<correctAns){
+                bandScore =  8.5
+            } 
+            if(38<correctAns){
+                bandScore =  9
+            } 
+        alert(correctAns +" answers is correct. Your band score is: " + bandScore ); 
+        location.reload();
+    });
+
+    $('#clear').click(function () { 
+        console.log('clear');
+        $('input:checkbox').prop('checked', false); ;
+    });
+});
